88b11d37089d04c39cd23d295b9da06b2b1c62e6
Gerardo Ramos <113146258+gramoslt@users.noreply.github.com>
2023-08-07 13:31:31 -0600

    Uploaded WordScramble completed project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Gerardo Leal on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
