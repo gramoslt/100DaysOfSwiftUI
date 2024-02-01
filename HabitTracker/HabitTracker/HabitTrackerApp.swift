289d973eb90d890eddec653066f14aa5a705ba6e
Gerardo Ramos <gerx_37@hotmail.com>
2024-02-01 12:06:33 -0600

    Created HabitTracker Project

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Gerardo Ramos on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
