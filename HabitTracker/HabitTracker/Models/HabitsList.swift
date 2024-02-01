2145bb77cc56f291f83d43c4f8b184e4890eb88d
Gerardo Ramos <gerx_37@hotmail.com>
2024-02-01 12:41:19 -0600

    Updated Habits models

--- HabitTracker/HabitTracker/Models/HabitsList.swift
+++ HabitTracker/HabitTracker/Models/HabitsList.swift
@@ -7,17 +7,24 @@
 
 import Foundation
 
-struct Habit: Identifiable {
+struct Habit: Identifiable, Hashable {
     let id: UUID = UUID()
     var name: String
     var frequency: Int
 }
 
-@Observable
-class HabitsList {
+class HabitsList: ObservableObject, Hashable {
+    static func == (lhs: HabitsList, rhs: HabitsList) -> Bool {
+        lhs.habits == rhs.habits
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(habits)
+    }
+
     var habits: [Habit]
 
-    init(habits: [Habit]) {
+    init(habits: [Habit] = [Habit]()) {
         self.habits = habits
     }
 }
