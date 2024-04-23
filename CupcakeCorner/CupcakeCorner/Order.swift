961d98c54b53ba65f747bf3f76826f333432dd3d
Gerardo Ramos <gerx_37@hotmail.com>
2024-04-23 09:14:26 -0600

    Created Order model

---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Gerardo Ramos on 22/04/24.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    var type = 0
+    var quantity = 3
+
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
